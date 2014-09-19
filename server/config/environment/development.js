c22147b333bd850e442471114a15a45c9c9f22a3
Daniel Streit <Daniel.Streit@gmail.com>
2014-09-19 10:45:01 -0700

    Initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/whatisit-dev'
+  },
+
+  seedDB: true
+};
